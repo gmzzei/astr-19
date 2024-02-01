7fb2f270448a9bcb68d4aaac41c90021242b1192
gianna mazzei <142944820+gmzzei@users.noreply.github.com>
2024-02-01 00:10:51 -0800

    session 2 prompt

---
+++ session2prompt.py
@@ -0,0 +1,24 @@
+# sum of two floating point numbers; print data type of resulting answer
+a = 2.5
+b = 7.5
+
+print("a + b =", a+b)
+c = a+b
+print("data type of result:", type(c))
+
+# the difference between two integers; print data type of resulting answer
+x = 4
+y = 6
+
+print("x - y =", x-y)
+z = x-y
+print("data type of result:", type(z))
+
+# the product of a floating point number and an integer; print data type of resulting answer
+
+r = 2
+s = 3.14159
+
+print("r x s =", r*s)
+t = r*s
+print("data type of result:", type(t))
