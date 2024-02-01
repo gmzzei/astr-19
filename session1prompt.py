bcf8960345f09c1628ee9a884bb2960cc3790905
gianna mazzei <142944820+gmzzei@users.noreply.github.com>
2024-02-01 00:16:50 -0800

    Update session1prompt.py

--- session1prompt.py
+++ session1prompt.py
@@ -1,4 +1,4 @@
 print("My name is Gianna.")
 print("My pronouns are she/her")
-print("My favorite movies is Forrest Gump because it reminds me of my childhood.")
+print("My favorite movie is Forrest Gump because it reminds me of my childhood.")
 print("My favorite food is sushi, specifically salmon nigiri with lemon on top!")
