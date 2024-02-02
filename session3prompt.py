c2fb9754ab86ca669c88205adf70b29aa8a7142f
gianna mazzei <142944820+gmzzei@users.noreply.github.com>
2024-02-01 19:05:36 -0800

    session 3 prompt

---
+++ session3prompt.py
@@ -0,0 +1,26 @@
+# Write a Python program that defines a function f(x) that returns x**3 + 8. In the main function of the program,
+# call f(x) with x = 9 and print the result.  Use an if statement that executes if the result is larger than 27 and
+# prints “YAY!”.
+
+# def main()
+    # instructions to be done
+    # in the main function
+    # with an indent
+
+# if __name__ == __"main"__:
+    # main()
+
+def main():
+    x = 9
+    y = x**3 + 8
+    print(y)
+
+    if y > 27:
+        print("YAY!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
